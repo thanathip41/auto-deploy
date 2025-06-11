21c2a60b6440a8c794cef815fa86b8dc80504060
thanathip srisawatpattana <thanathip.sr@irecruit.co.th>
2025-06-11 18:05:33 +0700

    release: version 1.0.2

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const http = require('http');
+const { version } = require('./package.json');
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.end('test:Hello from Docker!');
+  res.end(`release: Hello from Docker local!, Now Version: ${version}`);
 });
 
 server.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}, version ${version}`);
 });
