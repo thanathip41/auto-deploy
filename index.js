bbdbc7eea4d4cc0040c7fbfe09fbd33e332e1539
thanathip srisawatpattana <thanathip.sr@irecruit.co.th>
2025-06-11 16:31:39 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.end('Hello from Docker!');
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
